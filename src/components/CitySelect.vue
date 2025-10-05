d4d3f4d38effeafaa19ec4810daf191c4a67a6e1
eiskandarov <eiskandarov@gmail.com>
2025-10-05 19:54:23 +0500

    feat: add CitySelect component

---
+++ src/components/CitySelect.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Button from "./Button.vue";
+import IconLocation from "./icons/IconLocation.vue";
+
+const emit = defineEmits({
+  selectCity(payload) {
+    return payload ? true : false;
+  },
+});
+
+function select() {
+  emit("selectCity", "London");
+}
+</script>
+
+<template>
+  <Button @click="select()">
+    <IconLocation />
+    Изменить город
+  </Button>
+  <input />
+  <Button>Сохранить</Button>
+</template>
+
+<style scoped></style>
