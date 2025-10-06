cf1b0260a22183cce52203eff0367564e28fa95b
eiskandarov <eiskandarov@gmail.com>
2025-10-07 02:06:15 +0500

    fix: update city select component

--- src/components/CitySelect.vue
+++ src/components/CitySelect.vue
@@ -1,6 +1,8 @@
 <script setup>
+import { onMounted, ref } from "vue";
 import Button from "./Button.vue";
 import IconLocation from "./icons/IconLocation.vue";
+import Input from "./Input.vue";
 
 const emit = defineEmits({
   selectCity(payload) {
@@ -8,18 +10,43 @@ const emit = defineEmits({
   },
 });
 
+let isEdited = ref(false);
+let city = ref("Moscowa");
+
+onMounted(() => {
+  emit("selectCity", city.value);
+});
+
 function select() {
-  emit("selectCity", "London");
+  isEdited.value = false;
+  emit("selectCity", city.value);
+}
+
+function edit() {
+  isEdited.value = true;
 }
 </script>
 
 <template>
-  <Button @click="select()">
-    <IconLocation />
-    Изменить город
-  </Button>
-  <input />
-  <Button>Сохранить</Button>
+  <div class="city-select">
+    <div v-show="isEdited" class="city-input">
+      <Input v-model="city" placeholder="Москва" @keyup.enter="select()" />
+      <Button @click="select()">Сохранить</Button>
+    </div>
+    <Button v-show="!isEdited" @click="edit()">
+      <IconLocation />
+      Изменить город
+    </Button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.city-input {
+  display: flex;
+  column-gap: 12px;
+}
+
+.city-select {
+  width: 420px;
+}
+</style>
