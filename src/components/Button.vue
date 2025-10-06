75a0c5bd8b5479eb727920b96534d129ac971df4
eiskandarov <eiskandarov@gmail.com>
2025-10-07 02:05:29 +0500

    fix: update button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -10,11 +10,13 @@
 .button {
   display: flex;
   column-gap: 9px;
+  width: 100%;
+  justify-content: center;
   align-items: center;
   padding: 14px;
   font-size: 20px;
   font-weight: 500;
-  border-radius: 10px;
+  border-radius: var(--default-border-radius);
   letter-spacing: 0.4px;
   border: none;
   color: var(--color-primary);
