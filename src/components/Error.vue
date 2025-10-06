34a6f802371bc512b9282cf77b8fcd3b35dcbf15
eiskandarov <eiskandarov@gmail.com>
2025-10-07 02:05:37 +0500

    feat: add error component

---
+++ src/components/Error.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  error: String,
+});
+</script>
+
+<template>
+  <div v-if="props.error" class="error">{{ props.error }}</div>
+</template>
+
+<style scoped>
+.error {
+  --margin: 60px;
+  font-size: 18px;
+  padding: 15px;
+  text-align: center;
+  background-color: var(--color-card-bg);
+  border-radius: 0px 0px 25px 25px;
+  margin-top: calc(-1 * var(--margin));
+  margin-bottom: var(--margin);
+}
+</style>
