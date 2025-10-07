cdadf33b1b6ef70b7e1840605f0001e256a615da
eiskandarov <eiskandarov@gmail.com>
2025-10-07 13:33:14 +0500

    feat: add Day Card component

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,20 @@ import { computed, ref } from "vue";
 import CitySelect from "./components/CitySelect.vue";
 import Stat from "./components/Stat.vue";
 import Error from "./components/Error.vue";
+import DayCard from "./components/DayCard.vue";
 
 const API_ENDPOINT = "https://api.weatherapi.com/v1";
 const errorMap = new Map([[1006, "Указанный город не найден"]]);
 
 let data = ref();
 let error = ref();
+let activeIndex = ref(0);
 
 const errorDisplay = computed(() => {
   return errorMap.get(error.value?.error?.code);
 });
 
 const dataModified = computed(() => {
-  if (!data.value) {
-    return [];
-  }
-
   return [
     {
       label: "Влажность",
@@ -40,11 +38,13 @@ async function getCity(city) {
     q: city,
     lang: "ru",
     key: "a8ab481e6f044e13963192750250610",
-    days: 3,
+    days: 4,
   });
+
   const response = await fetch(
     `${API_ENDPOINT}/forecast.json?${params.toString()}`,
   );
+
   if (response.status != 200) {
     error.value = await response.json();
     data.value = null;
@@ -59,8 +59,29 @@ async function getCity(city) {
 <template>
   <main class="main">
     <Error :error="errorDisplay" />
-    <Stat v-for="item in dataModified" v-bind="item" :key="item.label" />
-    <CitySelect @select-city="getCity" />
+    <div class="stat-data">
+      <div v-if="data">
+        <div class="stat-list">
+          <Stat v-for="item in dataModified" v-bind="item" :key="item.label" />
+        </div>
+        <div class="day-card-list">
+          <DayCard
+            v-for="(item, index) in data.forecast.forecastday"
+            :key="item.date"
+            :is-active="activeIndex === index"
+            :weather-code="item.day.condition.code"
+            :temperature="item.day.avgtemp_c"
+            :date="new Date(item.date)"
+            @click="
+              () => {
+                activeIndex = index;
+              }
+            "
+          />
+        </div>
+      </div>
+      <CitySelect @select-city="getCity" />
+    </div>
   </main>
 </template>
 
@@ -69,5 +90,23 @@ async function getCity(city) {
   background-color: var(--color-bg-main);
   padding: 60px 50px;
   border-radius: 25px;
+}
+
+.stat-data {
+  display: flex;
+  flex-direction: column;
+  row-gap: 80px;
+}
+
+.stat-list {
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+  margin-bottom: 50px;
+}
+
+.day-card-list {
+  display: flex;
+  column-gap: 2px;
 }
 </style>
---
+++ src/components/DayCard.vue
@@ -0,0 +1,73 @@
+<script setup>
+import IconRain from "./icons/weather/IconRain.vue";
+import IconSun from "./icons/weather/IconSun.vue";
+
+const TEMPERATURE_MEASURE = "℃";
+const { weatherCode, temperature, date } = defineProps({
+  weatherCode: Number,
+  temperature: Number,
+  date: Date,
+  isActive: Boolean,
+});
+
+function getIconColor(isActive) {
+  return isActive ? "var(--color-primary-inverted)" : "var(--color-primary";
+}
+</script>
+
+<template>
+  <button type="button" class="day-card" :class="{ active: isActive }">
+    <IconSun v-if="weatherCode <= 1003" :color="getIconColor(isActive)" />
+    <IconRain
+      v-if="weatherCode >= 1006 && weatherCode <= 1063"
+      :color="getIconColor(isActive)"
+    />
+    <IconCloud v-if="weatherCode >= 1063" :color="getIconColor(isActive)" />
+    <div class="day-card__day">
+      {{ date.toLocaleDateString("ru-RU", { weekday: "short" }) }}
+    </div>
+    <div class="day-card__temperature">
+      {{ temperature }} {{ TEMPERATURE_MEASURE }}
+    </div>
+  </button>
+</template>
+
+<style scoped>
+.day-card {
+  font-size: 20px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 24px;
+  border-radius: 10px;
+  border: none;
+  color: var(--color-primary);
+  background-color: var(--color-card-bg);
+  cursor: pointer;
+}
+
+.day-card.active {
+  background-color: var(--color-primary);
+  color: var(--color-primary-inverted);
+}
+
+.day-card:not(.active):hover {
+  background-color: #3a434f;
+}
+
+.day-card__day {
+  font-style: normal;
+  font-weight: 700;
+}
+
+.day-card__day::first-letter {
+  text-transform: uppercase;
+}
+
+.day-card__temperature {
+  font-style: normal;
+  font-weight: 700;
+}
+</style>
